d8811daca3f8d40ed9d3f4ad1d4e2ea349824e3d
jm47895 <jordan.madrigal@gmail.com>
2020-03-06 13:57:19 -0600

    Add director, instructor, student flavors

--- app/src/main/java/com/example/dancertrack/MainActivity.kt
+++ app/src/main/java/com/example/dancertrack/MainActivity.kt
@@ -2,11 +2,15 @@ package com.example.dancertrack
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val textView : TextView = findViewById(R.id.instructor_text)
+        textView.text = "This is instructor text"
     }
 }
